4fba7720804d9a18840254b69b52127feee9457e
Kristjan Lääne <xonproject@gmail.com>
2024-06-27 20:27:23 +0300

    save user to neon db

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -3,25 +3,26 @@ import { db } from '@/lib/db';
 
 export const checkUser = async () => {
   const user = await currentUser();
+  console.log(user);
 
   // Check for current logged in clerk user
   if (!user) {
     return null;
   }
 
-  // Check if the usser is already in db
+  // Check if the user is already in the database
   const loggedInUser = await db.user.findUnique({
     where: {
       clerkUserId: user.id,
     },
   });
 
-  // If user is in db, then return user
+  // If user is in database, return user
   if (loggedInUser) {
     return loggedInUser;
   }
 
-  // If not in db, create new user
+  // If not in database, create new user
   const newUser = await db.user.create({
     data: {
       clerkUserId: user.id,
