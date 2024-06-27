98b18601bb3f7ad8cbf3112d206202d3f92bae1c
Kristjan Lääne <xonproject@gmail.com>
2024-06-27 20:08:28 +0300

    header

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <h2>Expense Tracker</h2>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
