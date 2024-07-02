5161baeba21539d3ba01af943a9acfb638e71545
Kristjan Lääne <xonproject@gmail.com>
2024-07-02 17:54:52 +0300

    getUserBalance

---
+++ components/Balance.tsx
@@ -0,0 +1,12 @@
+import getUserBalance from '@/app/actions/getUserBalance';
+
+const Balance = () => {
+  return (
+    <>
+      <h4>Your Balance:</h4>
+      <h1>$500</h1>
+    </>
+  );
+};
+
+export default Balance;
