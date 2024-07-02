0285658105efe6e3f7a10298cd0a7a2c8e7215f5
Kristjan Lääne <xonproject@gmail.com>
2024-07-02 18:09:45 +0300

    addCommas unility

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,10 +1,12 @@
 import getUserBalance from '@/app/actions/getUserBalance';
 
-const Balance = () => {
+const Balance = async () => {
+  const { balance } = await getUserBalance();
+
   return (
     <>
       <h4>Your Balance:</h4>
-      <h1>$500</h1>
+      <h1>${balance ?? 0}</h1>
     </>
   );
 };
